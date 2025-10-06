036082919cc8ec6c018ed2d93ac05be06218c7ba
Diksha <diksha@macbookpro>
2025-10-05 21:55:42 -0700

    updating

--- movie.cpp
+++ movie.cpp
@@ -25,25 +25,56 @@ public:
     //setters
     void setTitle(string t)	{title = t;}
 
-    // getters: no arguments, returns the stored values of the instance variables
-    double getRating()		{ return getRating(review); }
-    string getComment()		{ return getComment(review); }
-    int getBlue()               { return blue; }
+    //review methods
+    void addReview(double rating, string comment) {
+        Node* n = new Node;
+        n->rating = rating;
+        n->comment = comment;
+        n->next = head;
+        head = n;
+    }
+   
+    void displayReviews() {
+        cout << "\nMovie: " << title << endl;
+        if (!head) {
+            cout << "No reviews available.\n";
+            return;
+        }
 
+        Node* temp = head;
+        double total = 0;
+        int count = 1;
+        while (temp) {
+            cout << "\t> Review #" << count++ << ": " << temp->rating << ": " << temp->comment << endl;
+            total += temp->rating;
+            temp = temp->next;
+        }
+
+        cout << "\t> Average rating: " << total/(count-1) << "\n";
+    }
 }
+
+//randomRating() creates a random double rating between 1.0-5.0
+//returns the random double that was created
+double randomRating();
 
 
 int main(){
 
     array<Movie,10> movies;
-
+    
+    ifstream fin("reviews.txt");
+    if (!fin) {
+        cerr << "Error: Could not open reviews.txt\n";
+        return 1;
+    }
 
 }
 
-double getRating(Node *n, double place){
-    n->rating;
-    
-    
+double randomRating() {
+    int min = 10;   //1.0
+    int max = 50;   //5.0
+    int randomNum = rand() % (max-min+1)+min; //random between 10 and 50
+    double rating = randomNum / 10.0;               //one decimal place
+    return rating;
 }
-
-string getComment(Node *n)	{return n->comment;}
