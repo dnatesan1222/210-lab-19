ccd206dc9a4707fcc6b1df7677e525a869b40e15
Diksha <diksha@macbookpro>
2025-10-05 21:36:21 -0700

    updating

--- movie.cpp
+++ movie.cpp
@@ -14,18 +14,23 @@ struct Node{
 class Movie{
 private:
     string title;
-    Node *list;
+    Node *review;
 public:
-    
+    // getters: no arguments, returns the stored values of the instance variables
+    int getRed()                { return getRating(); }
+    int getGreen()              { return green; }
+    int getBlue()               { return blue; }
 
 }
 
 
 int main(){
 
-    
-
+    array<Movie,10> movies;
 
 
 }
 
+double getRating(Node *n)	{return n->rating;)
+
+string getComment(Node *n)	{return n->comment;}
