44911eccfa0acdb3a1a7d4368c244a7a14ea7e12
Diksha <diksha@macbookpro>
2025-10-05 18:12:34 -0700

    starting work with movie class

--- movie.cpp
+++ movie.cpp
@@ -1,4 +1,15 @@
 // COMSC-210 | Lab 17 | Diksha Tara Natesan
 // IDE used: Vim/Terminal
 
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+class Movie{
+
+
+
+}
+
 
